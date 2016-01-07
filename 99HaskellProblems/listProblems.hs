3a2e093e038cff16e11da749c491d201d6c2be79
Thomas Schneider <tgschneider53@gmail.com>
2016-01-06 19:36:29 -0800

    Problems through 16

--- 99HaskellProblems/listProblems.hs
+++ 99HaskellProblems/listProblems.hs
@@ -66,3 +66,87 @@ pack all@(x:xs) = (takeWhile (== x) all) : pack (dropWhile (== x) xs)
 
 encode :: (Eq a) => [a] -> [(Int, a)]
 encode xs = map (\xs -> (length xs, head xs)) $ pack xs
+
+-- Problem 11 --
+
+data Element a = Single a | Multiple Int a deriving (Show)
+
+encodeOne :: [a] -> Element a
+encodeOne [x] = Single x
+encodeOne xs = Multiple (length xs) (head xs)
+
+encodeModified :: (Eq a) => [a] -> [Element a]
+encodeModified xs = map encodeOne $ pack xs
+
+-- Problem 12 --
+
+decodeModified :: (Eq a) => [Element a] -> [a]
+decodeModified xs = concat $ map decodeOne xs
+    where
+        decodeOne (Single x) = [x]
+        decodeOne (Multiple l x) = replicate l x
+
+-- Problem 13 --
+
+-- Not sure what the problem statement means.
+
+-- Problem 14 --
+
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = x:x:(dupli xs)
+
+-- Problem 15 --
+
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli (x:xs) n = replicate n x ++ repli xs n
+
+-- Problem 16 --
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n
+    | length xs < n  = xs
+    | length xs == n = init xs
+    | otherwise      = take (n-1) xs ++ dropEvery (drop n xs) n
+
+-- Problem 17 --
+
+
+-- Problem 18 --
+
+
+-- Problem 19 --
+
+
+-- Problem 20 --
+
+
+-- Problem 21 --
+
+
+-- Problem 22 --
+
+
+-- Problem 23 --
+
+
+-- Problem 24 --
+
+
+-- Problem 25 --
+
+
+-- Problem 26 --
+
+
+-- Problem 27 --
+
+
+-- Problem 28 --
+
+
+-- Problem 29 --
+
+
+-- Problem 30 --
